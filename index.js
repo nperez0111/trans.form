a9ab87cb53c67257159f56cf376f20ea9b52cc40
Nick The Sick <nperez0111@gmail.com>
2016-08-31 20:03:26 -0700

    made a mutable version that allows chaining

--- index.js
+++ index.js
@@ -2,8 +2,42 @@
 
 const trans = require( "objextender" )( {
     form: function ( getCurrentObject, transformerObject ) {
+
         return require( 'object-manip' )( transformerObject, getCurrentObject() )
+
+    },
+    save: function ( getCurrentObject, transformerObject ) {
+
+        let addDiff = function ( that, obj ) {
+
+            Object.keys( obj ).forEach( function ( cur ) {
+
+                if ( ( obj[ cur ] ) === Object( obj[ cur ] ) ) {
+
+                    if ( !that.hasOwnProperty( cur ) || !isObj( that[ cur ] ) ) {
+
+                        that[ cur ] = {};
+                    }
+
+                    addDiff( that[ cur ], obj[ cur ] )
+
+                } else {
+
+                    that[ cur ] = obj[ cur ];
+
+                }
+
+            } )
+
+        }
+
+        addDiff.call( this, this, require( 'object-manip' )( transformerObject, getCurrentObject() ) )
+
+        return this
     }
 } )
 
-module.exports = trans.form;
+module.exports = {
+    toString: () => trans.form,
+    save: trans.save
+}
